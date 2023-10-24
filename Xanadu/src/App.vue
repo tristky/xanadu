2b49eaed4e1f6135fd14a35ee4b83fc27e3e54fe
brian-h123 <134823170+brian-h123@users.noreply.github.com>
2023-10-24 16:14:54 +0800

    App NavBar
    change to using navbar component in app.vue

--- Xanadu/src/App.vue
+++ Xanadu/src/App.vue
@@ -6,10 +6,7 @@ import NavBar from '@/components/NavBar.vue'
 <template>
   <div>
     <div id="nav">
-      <router-link to="/">Dashboard</router-link> |
-      <router-link to="/forum">Forum</router-link> |
-      <router-link to="/marketplace">Marketplace</router-link> |
-      <router-link to="/register">Register</router-link>
+      <NavBar/>
     </div>
     <router-view />
   </div>
